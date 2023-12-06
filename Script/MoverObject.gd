592161bbb181211c03e0c7fa38846e3add3a6aad
Moeva <mondohamoeva@hotmail.fr>
2023-12-06 08:32:19 +0100

    {coding}

--- Script/MoverObject.gd
+++ Script/MoverObject.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 
-var run_speed = 30.0
+var MAX_SPEED = 100
 var attacks = ["attack1", "attack2"]
 @onready var animationplayer = $AnimationPlayer
 @onready var animatedsprite = $AnimatedSprite2D
+var gravity = 50
 
 func _ready():
 	animatedsprite.play("idle")
 
 func _process(delta: float) -> void:
+	velocity.y += gravity * delta
 	if Input.is_action_just_pressed("move right"):
 		animationplayer.play("walk")
-		velocity += Vector2(run_speed * delta,0)
+		velocity += Vector2(MAX_SPEED * delta,0)
 		position = velocity * delta
-		move_and_slide()
+	move_and_slide()
